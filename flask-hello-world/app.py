6e3aaa3275add88c53b2ad72337d759f3214801d
Rajab Raza <mohammadrajabraza@gmail.com>
2020-04-02 01:18:28 +0500

    added connection string to app config

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/example'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
